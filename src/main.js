597c20a783fe49293e708da49a4d6e2f7fc0dc75
WhisperedPanic <adam.k.oliver@protonmail.com>
2026-04-22 19:06:20 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,10 +2,28 @@ import { state, setParams } from "./state/projectileState.js";
 import { analyze } from "./analysis/analyze.js";
 import { render } from "./render/svgRenderer.js";
 import { bindControls, populateInputs } from "./ui/controls.js";
+import { computeRequiredVelocityForStability } from "./ballistics/ballistics.js";
 
 function update(newParams) {
     setParams(newParams);
     state.result = analyze(state.params);
+
+    // ---- Sanity check calculation ----
+    const twist = +document.getElementById("twist_select").value;
+    const sg = +document.getElementById("sg_select").value;
+
+    const velocity = computeRequiredVelocityForStability(state.params, {
+        mass_gr: state.result.ballistics.mass_gr,
+        twist_in: twist,
+        stability: sg
+    });
+
+    state.sanity = {
+        twist,
+        sg,
+        velocity
+    };
+
     render(state);
 }
 
