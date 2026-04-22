9332a59ae03c178738a5926fbfe503bd32fda540
WhisperedPanic <adam.k.oliver@protonmail.com>
2026-04-22 21:01:39 +0100

    Update analyze.js

--- src/analysis/analyze.js
+++ src/analysis/analyze.js
@@ -1,18 +1,28 @@
 import { computeProjectileOutline, computeNoseProfile } from "../geometry/geometry.js";
-import { computeBallistics, computeCenterOfMass } from "../ballistics/ballistics.js";
+import {
+    integrateProjectile,
+    computeBallistics,
+    computeCenterOfPressure
+} from "../ballistics/ballistics.js";
 
 export function analyze(params) {
-    const ballistics = computeBallistics(params);
-    const com = computeCenterOfMass(params);
+    const outline = computeProjectileOutline(params);
+
+    const { volume, com, inertia } = integrateProjectile(params);
+    const cp = computeCenterOfPressure(outline);
+
+    const ballistics = computeBallistics(params, volume);
 
     return {
         geometry: params,
         ballistics,
         stability: {
-            center_of_mass: com
+            center_of_mass: com,
+            center_of_pressure: cp,
+            inertia
         },
         points: {
-            outline: computeProjectileOutline(params),
+            outline,
             nose: computeNoseProfile(params)
         }
     };
