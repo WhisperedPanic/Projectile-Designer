fe5182e985b49d15521c650c54b64113f2ca922a
WhisperedPanic <adam.k.oliver@protonmail.com>
2026-04-22 19:19:08 +0100

    Update svgRenderer.js

--- src/render/svgRenderer.js
+++ src/render/svgRenderer.js
@@ -43,6 +43,16 @@ export function render(state) {
     // ---- Output JSON ----
     document.getElementById("output").textContent =
         JSON.stringify(state.result, null, 2);
+
+    // ---- SG Check ----
+    const sanity = state.sanity;
+
+    if (sanity) {
+        document.getElementById("sanity_output").textContent =
+            `Twist: 1:${sanity.twist}" | ` +
+            `Target Sg: ${sanity.sg} | ` +
+            `Required Velocity: ${sanity.velocity.toFixed(0)} fps`;
+    }
 }
 
 
@@ -71,13 +81,4 @@ function drawCOM(svg, comX, scale, offsetX, offsetY) {
 
     svg.appendChild(line1);
     svg.appendChild(line2);
-}
-
-const sanity = state.sanity;
-
-if (sanity) {
-    document.getElementById("sanity_output").textContent =
-        `Twist: 1:${sanity.twist}" | ` +
-        `Target Sg: ${sanity.sg} | ` +
-        `Required Velocity: ${sanity.velocity.toFixed(0)} fps`;
 }
