b67ae9cdea850bd76a169e333ac45536dc0e2dbe
WhisperedPanic <adam.k.oliver@protonmail.com>
2026-04-22 15:12:37 +0100

    Create svgRenderer.js

---
+++ src/render/svgRenderer.js
@@ -0,0 +1,26 @@
+export function render(state) {
+    const svg = document.getElementById("canvas");
+    svg.innerHTML = "";
+
+    const pts = state.result.points.outline;
+
+    const scale = 400;
+    const offsetX = 50;
+    const offsetY = 100;
+
+    const path = pts.map((p,i)=>
+        (i===0?"M":"L") +
+        (p.x*scale+offsetX)+","+
+        (-p.y*scale+offsetY)
+    ).join(" ");
+
+    const el = document.createElementNS("http://www.w3.org/2000/svg","path");
+    el.setAttribute("d", path);
+    el.setAttribute("stroke","lime");
+    el.setAttribute("fill","none");
+
+    svg.appendChild(el);
+
+    document.getElementById("output").textContent =
+        JSON.stringify(state.result, null, 2);
+}
